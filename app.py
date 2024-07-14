d570bc008968b446d9a029c4b57858d8e8f7a9e0
anik2118 <anikghoshr123@gmail.com>
2024-07-14 16:15:46 +0600

    final

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 # Serve a default page. This function is not required. Serving up a spy.gif for the homepage.
 @app.route('/')
+
 def my_function():
+    global count
     spy_meme = "tracker.png"
     count+=1
     return send_file(spy_meme, mimetype="image/gif")
